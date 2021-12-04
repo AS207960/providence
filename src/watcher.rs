44d8155a318a0c81d2d3e08644d34efeb8e88eda
Q <q@misell.cymru>
2021-12-04 01:17:14 +0000

    bind tree queue

--- src/watcher.rs
+++ src/watcher.rs
@@ -82,7 +82,7 @@ impl<S: 'static + CTLogStorage + std::marker::Send + Clone> CTWatcher<S> {
                     0
                 });
 
-                let (entry_tx, entry_rx) = std::sync::mpsc::channel();
+                let (entry_tx, entry_rx) = std::sync::mpsc::sync_channel(100);
                 let mut new_tree = self.tree.clone();
                 let log_name = self.log.name.clone();
                 let log_id = self.log.id.clone();
@@ -180,7 +180,7 @@ impl<S: 'static + CTLogStorage + std::marker::Send + Clone> CTWatcher<S> {
                         self.evt_sender.send(crate::CTEvent {
                             entry,
                             log: self.log.clone()
-                        }).expect("Unable to send evennt");
+                        }).expect("Unable to send event");
                     }
                 } else {
                     std::mem::drop(emmit_entries);
